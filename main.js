9737163540dd24f225471f067cbc3ce70c48121c
taewook kwak <93242268+TaewookKwak@users.noreply.github.com>
2021-11-19 19:37:58 +0900

    JS-Home contact button

--- main.js
+++ main.js
@@ -4,8 +4,8 @@
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () =>{
-    console.log(window.scrollY);
-    console.log(`navbar height :${navbarHeight}`);
+    //console.log(window.scrollY);
+    //console.log(`navbar height :${navbarHeight}`);
     if(window.scrollY > navbarHeight){
         navbar.classList.add('navbar--dark')
     }else{
@@ -15,13 +15,23 @@ document.addEventListener('scroll', () =>{
 
 // Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
-navbarMenu,addEventListener('click', (event) =>{
+navbarMenu.addEventListener('click', (event) =>{
     const target = event.target;
     const link = target.dataset.link;
     if(link == null){
         return;
     }
     console.log(event.target.dataset.link);
-    const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView({behavior: 'smooth'});
+    scrollIntoView(link);
 })
+
+// Handle click on "contact me" button on home
+const homeContactBtn = document.querySelector('.home__contact');
+homeContactBtn.addEventListener('click', (event)=> {
+    scrollIntoView('#contact');
+})
+
+function scrollIntoView(selector){
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior: 'smooth'});
+}
