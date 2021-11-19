e9c37c3e8126ded5fb3323bcbf8dbcc77661537f
taewook kwak <93242268+TaewookKwak@users.noreply.github.com>
2021-11-19 18:57:24 +0900

    JS-Navbar menu click

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 'use strict'
 
-
+//Make navbar transparent when it is on the top
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () =>{
@@ -11,4 +11,17 @@ document.addEventListener('scroll', () =>{
     }else{
         navbar.classList.remove('navbar--dark')
     }
+})
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu,addEventListener('click', (event) =>{
+    const target = event.target;
+    const link = target.dataset.link;
+    if(link == null){
+        return;
+    }
+    console.log(event.target.dataset.link);
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: 'smooth'});
 })
