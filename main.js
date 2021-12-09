7719772242eabbeeb2e0af06b7c0ee88c3cc22d6
taewook kwak <93242268+TaewookKwak@users.noreply.github.com>
2021-12-09 14:15:37 +0900

    1

--- main.js
+++ main.js
@@ -19,6 +19,7 @@ const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) =>{
     const target = event.target;
     const link = target.dataset.link;
+    console.log(link);
     if(link == null){
         return;
     }
